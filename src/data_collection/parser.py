1af7e645b05b84a64688a6f951c7d47926647217
Prasoon Varshney <pvarshne@andrew.cmu.edu>
2022-10-18 18:01:27 -0400

    minor fixes to parser

--- src/data_collection/parser.py
+++ src/data_collection/parser.py
@@ -1,8 +1,12 @@
 import json
+import lxml
 import os
 import re
 import spacy
 import scipdf
+import random
+import logging
+from tqdm import tqdm
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -28,7 +32,11 @@ def add_stop(text):
         return text + '.'
 
 def parse_pdf(pdf_url): 
-    article = scipdf.parse_pdf_to_dict(pdf_url, as_list=False)
+    try:
+        article = scipdf.parse_pdf_to_dict(pdf_url, as_list=False)
+    except Exception as e:
+        logging.error(f"Failed to parse PDF {pdf_url} with exception {e}")
+        return None
 
     full_text = " ".join([add_stop(article['title']), add_stop(article['abstract'])])
     for section in article['sections']: 
@@ -40,25 +48,30 @@ def parse_pdf(pdf_url):
     full_text_tokenized = [preprocess(line) for line in re.split(sentence_splitting_regex, full_text)]
     full_text_tokenized = [text for text in full_text_tokenized if text is not None]
 
-    file_name = os.path.join(FILE_PATH, url.split('/')[-1] + ".txt")
+    file_name = os.path.join(FILE_PATH, pdf_url.split('/')[-1] + ".txt")
 
     with open(file_name, 'w') as f: 
         f.writelines(full_text_tokenized)
 
     parsed_article_dict = {
-        "url": url,
+        "url": pdf_url,
         "file_name": file_name
     }
     return parsed_article_dict
 
 
 if __name__ == "__main__":
-    all_urls = ['https://aclanthology.org/2022.acl-long.1.pdf', 'https://aclanthology.org/N19-1423.pdf']
+    with open("./src/data_collection/pdf_urls.txt", 'r') as f:
+        all_urls = f.readlines()
+
+    sampled_urls = random.choices(list(filter(lambda x: "aclanthology" in x, all_urls)), k=500)
 
     all_parsed_pdfs = []
-    for url in all_urls: 
+    for url in tqdm(sampled_urls): 
+        url = re.sub(r'\n', '', url)
         parsed_pdf = parse_pdf(url)
-        all_parsed_pdfs.append(parsed_pdf)
+        if parsed_pdf is not None: 
+            all_parsed_pdfs.append(parsed_pdf)
 
     # Save down summary of URLs and saved processed files
     summary = {"parsed_pds": all_parsed_pdfs}
