d1a349af9168ac3ab516ced249e863cb0f16b543
Prasoon Varshney <pvarshne@andrew.cmu.edu>
2022-10-21 14:58:32 -0400

    Sample 30 papers and add parsed outputs to data

--- src/data_collection/parser.py
+++ src/data_collection/parser.py
@@ -31,7 +31,17 @@ def add_stop(text):
     else: 
         return text + '.'
 
-def parse_pdf(pdf_url): 
+def parse_pdf(pdf_url, reparse_parsed_files=False): 
+    file_name = os.path.join(FILE_PATH, pdf_url.split('/')[-1] + ".txt")
+
+    if os.path.exists(file_name) and not reparse_parsed_files:
+        # If paper already parsed in a previous run, skip re-parsing, but return parsed file name
+        parsed_article_dict = {
+            "url": pdf_url,
+            "file_name": file_name
+        }
+        return parsed_article_dict
+
     try:
         article = scipdf.parse_pdf_to_dict(pdf_url, as_list=False)
     except Exception as e:
@@ -48,8 +58,6 @@ def parse_pdf(pdf_url):
     full_text_tokenized = [preprocess(line) for line in re.split(sentence_splitting_regex, full_text)]
     full_text_tokenized = [text for text in full_text_tokenized if text is not None]
 
-    file_name = os.path.join(FILE_PATH, pdf_url.split('/')[-1] + ".txt")
-
     with open(file_name, 'w') as f: 
         f.writelines(full_text_tokenized)
 
@@ -61,10 +69,17 @@ def parse_pdf(pdf_url):
 
 
 if __name__ == "__main__":
-    with open("./src/data_collection/pdf_urls.txt", 'r') as f:
-        all_urls = f.readlines()
+    with open("./src/data_collection/sampled_urls.txt", 'r') as f:
+        sampled_urls = f.readlines()
+ 
+    # sampled_urls = random.choices(list(filter(lambda x: "aclanthology" in x, all_urls)), k=500)
+    # year_list = ["aclanthology.org/2020", "aclanthology.org/2021", "aclanthology.org/2022", "aclanthology.org/P19", 
+    #     "aclanthology.org/P18", "aclanthology.org/P17", "aclanthology.org/P16", "aclanthology.org/P15"]
 
-    sampled_urls = random.choices(list(filter(lambda x: "aclanthology" in x, all_urls)), k=500)
+    # sampled_urls = []
+    # for year in year_list:
+    #     sampled_urls_by_year = random.choices(list(filter(lambda x: year in x, all_urls)), k=5)
+    #     sampled_urls = sampled_urls + sampled_urls_by_year
 
     all_parsed_pdfs = []
     for url in tqdm(sampled_urls): 
@@ -74,7 +89,7 @@ if __name__ == "__main__":
             all_parsed_pdfs.append(parsed_pdf)
 
     # Save down summary of URLs and saved processed files
-    summary = {"parsed_pds": all_parsed_pdfs}
+    summary = {"parsed_pdfs": all_parsed_pdfs}
     summary_json = json.dumps(summary, indent=4)
     with open(os.path.join(FILE_PATH, "summary.json"), 'w') as f: 
         f.write(summary_json)
